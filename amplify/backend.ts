3980e4fd80706b23abf4ddf26a8b088366b7e497
Frank Nguyen <hhn@amazon.com>
2025-02-27 19:38:01 -0800

    Add simple throttling

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -4,6 +4,7 @@ import { PolicyStatement } from "aws-cdk-lib/aws-iam";
 import { auth } from "./auth/resource";
 import { askAgent } from './functions/ask-agent/resource';
 import {preSignUp} from "./auth/pre-sign-up/resource";
+import {AttributeType, BillingMode, ProjectionType, TableEncryption, Table, CfnTable} from 'aws-cdk-lib/aws-dynamodb'
 
 const backend = defineBackend({
     auth,
@@ -27,9 +28,28 @@ backend.askAgent.resources.lambda.grantPrincipal.addToPrincipalPolicy(
     })
 );
 
+backend.askAgent.resources.lambda.grantPrincipal.addToPrincipalPolicy(
+    new PolicyStatement({
+        resources: ["*"],
+        actions: ["dynamodb:*"],
+    })
+);
+
 backend.preSignUp.resources.lambda.grantPrincipal.addToPrincipalPolicy(
     new PolicyStatement({
         resources: ["*"],
         actions: ["ssm:GetParameter"],
     })
 );
+
+const backendStack = backend.createStack("BackendStack");
+
+const table = new Table(backendStack, `AgentAccessLogDDBTable`, {
+    partitionKey: {name: 'user_name', type: AttributeType.STRING},
+    sortKey: {name: 'access_time', type: AttributeType.STRING},
+    tableName: `AgentAccessLog`,
+    pointInTimeRecovery: true,
+    billingMode: BillingMode.PAY_PER_REQUEST,
+    encryption: TableEncryption.AWS_MANAGED,
+    deletionProtection: true
+});
--- amplify/functions/ask-agent/handler.ts
+++ amplify/functions/ask-agent/handler.ts
@@ -1,9 +1,16 @@
 import type { Schema } from "../../data/resource";
 import { BedrockAgentRuntimeClient, InvokeAgentCommand } from "@aws-sdk/client-bedrock-agent-runtime";
 import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
+import { DynamoDBClient, PutItemCommand, QueryCommand } from "@aws-sdk/client-dynamodb";
+
+const RESPONSE_SERVER_SIDE_ERROR = "Something is wrong.";
+const RESPONSE_DEFAULT = "The answer to your question is 42.";
+const RESPONSE_RATE_LIMIT_EXCEEDED = "Request exceeds rate limit, please slow down.";
+const TABLE_NAME = "AgentAccessLog";
+const REGION = "us-east-1";
 
 const getParameter = async (key: string): Promise<string> => {
-    const client = new SSMClient({ region: "us-east-1" });
+    const client = new SSMClient({ region: REGION });
     const input = { Name: key, WithDecryption: false };
     const command = new GetParameterCommand(input);
     const response = await client.send(command);
@@ -12,10 +19,9 @@ const getParameter = async (key: string): Promise<string> => {
 }
 
 const invokeBedrockAgent = async (prompt: string, sessionId: string): Promise<string> => {
-    const client = new BedrockAgentRuntimeClient({ region: "us-east-1" });
     const agentId = await getParameter("BEDROCK_AGENT_ID");
     const agentAliasId = await getParameter("BEDROCK_AGENT_ALIAS_ID");
-    console.log(`Prepare to invoke Bedrock Agent: ${agentId}/${agentAliasId}, prompt: ${prompt}`);
+    console.log(`Prepare to invoke Bedrock Agent: ${agentId}/${agentAliasId}, prompt: ${prompt}, sessionId: ${sessionId}`);
 
     const command = new InvokeAgentCommand({
         agentId,
@@ -23,6 +29,7 @@ const invokeBedrockAgent = async (prompt: string, sessionId: string): Promise<st
         sessionId,
         inputText: prompt,
     });
+    const client = new BedrockAgentRuntimeClient({ region: REGION});
 
     try {
         let completion = "";
@@ -44,20 +51,19 @@ const invokeBedrockAgent = async (prompt: string, sessionId: string): Promise<st
         return completion;
     } catch (err: any) {
         console.error(err);
-        return "Something wrong";
+        return RESPONSE_SERVER_SIDE_ERROR;
     }
 };
 
-const getAnswer = async (question: string | undefined, sessionId: string | undefined): Promise<string> => {
-    if (question == undefined || sessionId == undefined) {
-        return "I don't know";
-    }
+const getAnswer = async (event: any): Promise<string> => {
+    const question = event.arguments.question;
+    const sessionId = event.arguments.sessionId;
 
     // If agent is not enabled, take a nap for some random seconds and return
     const bedrockAgentEnabled = await getParameter("BEDROCK_AGENT_ENABLED");
     if (bedrockAgentEnabled.toUpperCase() != "TRUE") {
-        await new Promise(f => setTimeout(f, 2000 + 3000 * Math.random()))
-        return "The answer to your question is 42."
+        await new Promise(f => setTimeout(f, 2000 + 3000 * Math.random()));
+        return RESPONSE_DEFAULT;
     }
 
     else {
@@ -65,11 +71,79 @@ const getAnswer = async (question: string | undefined, sessionId: string | undef
     }
 }
 
+const logUserAccess = async (event: any) => {
+    const user_name = event.identity.username;
+    const question = event.arguments.question;
+    const access_time = new Date().toISOString();
+    console.log(`Prepare to log access for user_name: ${user_name}, access_time: ${access_time} to table: ${TABLE_NAME}`);
+
+    const params = {
+        TableName: TABLE_NAME,
+        Item: {
+            user_name: { S: user_name },
+            access_time: { S: access_time},
+            question: { S: question},
+        },
+    };
+    const command = new PutItemCommand(params);
+    const client = new DynamoDBClient({ region: REGION });
+
+    try {
+        const response = await client.send(command);
+        console.log("Success", response);
+    } catch (error) {
+        console.error("Error", error);
+    }
+}
+
+// Implement simple rate limiting functionality, where each user cannot exceed {max_count} number of requests per {duration_in_minutes}.
+const isRateLimited = async (event: any, duration_in_minutes: number, max_count: number) => {
+    const user_name = event.identity.username;
+    const end_time = new Date();
+    const start_time = new Date(end_time);
+    start_time.setMinutes(end_time.getMinutes() - duration_in_minutes);
+    console.log(`Prepare query table: ${TABLE_NAME} with user_name: ${user_name}, access_time between: ${start_time.toISOString()} and: ${end_time.toISOString()}`);
+
+    const params = {
+        TableName: TABLE_NAME,
+        KeyConditionExpression: "user_name = :pk AND access_time BETWEEN :sk1 AND :sk2",
+        ExpressionAttributeValues: {
+            ":pk": { S: user_name },
+            ":sk1": { S: start_time.toISOString() },
+            ":sk2": { S: end_time.toISOString() }
+        },
+    };
+
+    const client = new DynamoDBClient({ region: "us-east-1" });
+    const command = new QueryCommand(params);
+
+    try {
+        const response = await client.send(command);
+        console.log(response.Items);
+        const rateLimited = response.Items!.length >= max_count;
+        console.log(`Found ${response.Items!.length} record(s). Applying max_count: ${max_count}, the returned value of isRateLimited is: ${rateLimited}`)
+        return rateLimited;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+
 export const handler: Schema["askAgent"]["functionHandler"] = async (event) => {
     console.log(`Received event: ${JSON.stringify(event)}`);
-    const question = event.arguments.question!;
-    const sessionId = event.arguments.sessionId!;
-    const answer = await getAnswer(question, sessionId);
+    const duration_in_minutes =  await getParameter("DURATION_IN_MINUTES");
+    const max_count = await getParameter("MAX_COUNT");
+    const rateLimited = await isRateLimited(event, Number(duration_in_minutes), Number(max_count));
+    let answer;
+
+    if (!rateLimited) {
+        await logUserAccess(event);
+        answer = await getAnswer(event);
+    }
+    else {
+        await new Promise(f => setTimeout(f, 1000));
+        answer = RESPONSE_RATE_LIMIT_EXCEEDED;
+    }
     console.log(`Returning response: ${answer}`);
 
     return answer;
